db49b9c196f0cad2fcf61d3efdc1d7b976a9a6ac
Samaneh <samaneh2010gh@gmail.com>
2021-07-24 15:38:40 -0400

    test get numbers of rows in a table

---
+++ web_table_get_rows_cols.py
@@ -0,0 +1,25 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+test_url = "https://www.w3schools.com/html/html_tables.asp"
+
+
+class WebTableTest(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+
+    def test_1_get_num_rows_(self):
+        driver = self.driver
+        driver.get(test_url)
+
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CLASS_NAME, "w3-example")))
+
+        num_rows = len(driver.find_elements_by_xpath("//*[@id='customers']/tbody/tr"))
+        print("Rows in table are ", num_rows)
